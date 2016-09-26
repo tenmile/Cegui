d21411554c54bc6830e8dba35283015dcf11fc2f
rjt <2015036119@qq.com>
2016-09-26 21:00:16 +0800

    nothing

--- include/EventArgs.h
+++ include/EventArgs.h
@@ -1,10 +1,15 @@
 #ifndef _EventArgs_h_
 #define _EventArgs_h_
-
 namespace
 {
-
+	
+class EventArgs
+{
+	EventArgs(void): handled(0){}
+	virtual ~EventArgs(void){}
+	
+	uint handled; //处理完事件后递增handled
 }
 
-
+}
 #endif
---
+++ src/EventArgs.cpp
@@ -0,0 +1,6 @@
+#include "EventArgs.h"
+
+namespace CEGUI
+{
+	
+}
